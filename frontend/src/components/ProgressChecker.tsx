42fb4b9f3a0692564503fd91df6c8d9863fec6dd
Cduff1174 <colinduffy1174@gmail.com>
2025-08-18 19:02:05 -0400

    Fixing the Progress Tracking and Updating App

---
+++ frontend/src/components/ProgressChecker.tsx
@@ -0,0 +1,13 @@
+// frontend/src/components/ProgressChecker.tsx
+
+import React from 'react';
+
+const ProgressChecker = () => {
+  return (
+    <div>
+      Progress Checker Coming Soon!
+    </div>
+  );
+};
+
+export default ProgressChecker;
